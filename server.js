11d7750cec3a49a37be7e623ea4da147525c68b0
benoit berlanger <benoitberlanger@MacBook-Pro-de-benoit.local>
2022-08-23 21:25:22 +0200

    Suite

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/user.routes");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db.js");
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//routes
+app.use("/api/user", userRoutes);
+
+//server
 app.listen(process.env.PORT, () => {
   console.log(`listening port ${process.env.PORT}`);
 });
